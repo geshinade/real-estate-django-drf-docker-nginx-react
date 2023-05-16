a291f4ef67fed787424d3951489ee4db37ba19f5
geshinade <106628927+geshinade@users.noreply.github.com>
2023-05-16 20:19:23 +0100

    chore: setup nginx and redux toolkit with docker

--- real_estate/settings/local.py
+++ real_estate/settings/local.py
@@ -10,7 +10,7 @@ SECRET_KEY = env(
     default="VoQE5G62Qu1Sk8cmBMa8V8D4nYhWazjaEoH9p9wWGPF4Pv23A3M68Wtme2BpHSwt",
 )
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "api"] 
 
 EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
